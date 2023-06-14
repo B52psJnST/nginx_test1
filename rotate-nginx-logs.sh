8a2f46be56efc5fb9094d14fae8d499c17ca96a3
B52psJnST <136075789+B52psJnST@users.noreply.github.com>
2023-06-14 11:27:33 +0800

    Create rotate-nginx-logs.sh

---
+++ rotate-nginx-logs.sh
@@ -0,0 +1,26 @@
+
+
+
+tee /root/scripts/rotate-nginx-logs.sh <<-'EOF'
+
+
+#!/bin/bash
+# Description: rotate nginx access logs
+#nginx日志切割脚本
+
+
+NGX_PID=`cat /usr/local/nginx/logs/nginx.pid`
+
+LOGS_DIR="/usr/local/nginx/logs"
+
+LOG1_NAME="main.log"
+LOG2_NAME="error.log"
+
+cd $LOGS_DIR
+/usr/bin/mv  $LOG1_NAME $(date +%F -d 'yesterday')_$LOG1_NAME
+/usr/bin/mv  $LOG2_NAME $(date +%F -d 'yesterday')_$LOG2_NAME
+
+/usr/bin/kill -USR1 $NGX_PID
+
+
+EOF
